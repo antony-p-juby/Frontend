0e589b80988b3d62dc2837c2a2664332c68c86f8
antony-p-juby <antonypjuby@gmail.com>
2025-08-10 13:49:56 +0530

    first commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,135 @@
+
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'config.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool _isNotValidate = false;
+
+  void loginUser() async {
+    if (emailcontroller.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      setState(() {
+        _isNotValidate = false;
+      });
+
+      var reqBody = {
+        "email": emailcontroller.text,
+        "password": passwordController.text,
+      };
+
+      final response = await http.post(
+        Uri.parse(login),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(reqBody),
+      );
+
+      var jsonResponse=jsonDecode(response.body);
+      if(jsonResponse['status']){
+
+      }else{
+        print('Error while checking credentials');
+      }
+
+    } else {
+      setState(() {
+        _isNotValidate = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [_header(context), _inputField(context)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "Welcome",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Enter your credentials to login"),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          controller: emailcontroller,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            hintText: "Username",
+            errorText: _isNotValidate
+                ? "Please provide a valid username"
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Colors.purple.withValues(alpha: 0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.person),
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          controller: passwordController,
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "Password",
+            errorText: _isNotValidate
+                ? "Please provide a valid password"
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Colors.purple.withValues(alpha: 0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.lock),
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: loginUser,
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.purple,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
